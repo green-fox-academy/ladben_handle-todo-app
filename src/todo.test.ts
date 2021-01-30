e05bb7b2a12f596782abb4227b4f675e6a132eb8
Ladányi Bence <ladanyi.bence96@gmail.com>
2021-01-30 14:55:32 +0100

    FileHandlers convert to string method works fine, but get Task Array fails

--- src/todo.test.ts
+++ src/todo.test.ts
@@ -1,16 +1,39 @@
 import * as test from 'tape';
 import { Task } from './task';
+import { List } from './list';
+import { FileHandler } from './file-handler';
+import { FILE } from 'dns';
 
 test('check a task', (t) => {
   const myTask: Task = new Task(1, 'Walk the dog');
-  myTask.print();
-  myTask.print('include checks');
   myTask.check();
-  myTask.print('include checks');
 
   const actual: boolean = myTask.checked;
   const expected: boolean = true;
 
   t.equal(actual, expected);
+  t.end();
+});
+
+test('convert string to task', (t) => {
+  const myFileHandler: FileHandler = new FileHandler('src/my-list.txt');
+
+  const actual: Task = myFileHandler.convertStringToTask('1\tTest this\tfalse');
+  const expected: Task = new Task(1, 'Test this', false);
+
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test('get taskArray', (t) => {
+  const myFileHandler: FileHandler = new FileHandler('src/my-list.txt');
+
+  const actual: Task[] = myFileHandler.getTaskArray();
+  const expected: Task[] = [
+    new Task(1, 'Test this', false),
+    new Task(2, 'Walk the dog', true),
+  ];
+
+  t.deepEqual(actual, expected);
   t.end();
 });
