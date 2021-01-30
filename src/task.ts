ba1b255fdc27c1a33e64125de03db3fe33015983
Ladányi Bence <ladanyi.bence96@gmail.com>
2021-01-30 13:50:08 +0100

    Task class done

---
+++ src/task.ts
@@ -0,0 +1,40 @@
+import { Checkable } from './interfaces/checkable';
+import { Printable } from './interfaces/printable';
+
+export class Task implements Checkable, Printable {
+  private position: number;
+  private description: string;
+  checked: boolean;
+
+  constructor(position: number, description: string, checked: boolean) {
+    this.position = position;
+    this.description = description;
+    this.checked = checked;
+  }
+
+  print(mode?: string): void {
+    if (!mode) {
+      console.log(`${this.position} - ${this.description}`);
+    } else if (mode === 'include checks') {
+      let checkIndicator: string;
+
+      if (this.checked) {
+        checkIndicator = 'X';
+      } else {
+        checkIndicator = ' ';
+      }
+
+      console.log(`${this.position} - [${checkIndicator}] ${this.description}`);
+    } else {
+      throw new Error('Mode can only be "include checks" if given.');
+    }
+  }
+
+  check(): void {
+    this.checked = true;
+  }
+
+  uncheck(): void {
+    this.checked = false;
+  }
+}
