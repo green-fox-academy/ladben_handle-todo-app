5a14b53720b6d3729300997e669b79b68e7a540c
Ladányi Bence <ladanyi.bence96@gmail.com>
2021-02-04 11:32:23 +0100

    file handler structure modification

--- src/file-handler.ts
+++ src/file-handler.ts
@@ -1,23 +1,18 @@
-import { Socket } from 'dgram';
 import * as fs from 'fs';
 import { Task } from './task';
 
 export class FileHandler {
   private source: string;
   private data: string;
-  private stringTaskArray: string[];
+  private linesOfData: string[];
 
   constructor(source: string) {
     this.source = source;
-    this.data = fs.readFileSync(source, 'utf-8').slice(0, -1);
-    this.stringTaskArray = this.data.split('\n');
+    this.data = fs.readFileSync(source, 'utf-8');
+    this.linesOfData = this.data.split('\n');
   }
 
-  getData(): string {
-    return this.data;
-  }
-
-  convertStringToTask(stringToConvert: string): Task {
+  private convertStringToTask(stringToConvert: string): Task {
     let stringArray: string[] = stringToConvert.split('\t');
     let position: number = parseInt(stringArray[0]);
     let description: string = stringArray[1];
@@ -26,72 +21,35 @@ export class FileHandler {
     return new Task(position, description, checked);
   }
 
+  private convertTaskToString(taskToConvert: Task): string {
+    return `${taskToConvert.getPosition()}\t${taskToConvert.getDescription()}\t${
+      taskToConvert.checked === true ? 'true' : 'false'
+    }`;
+  }
+
+  private updateData(): void {
+    this.data = fs.readFileSync(this.source, 'utf-8');
+    this.linesOfData = this.data.split('\n');
+  }
+
   getTaskArray(): Task[] {
+    this.updateData();
     let taskArray: Task[] = [];
 
-    for (let taskElement of this.stringTaskArray) {
+    for (let taskElement of this.linesOfData) {
       taskArray.push(this.convertStringToTask(taskElement));
     }
 
     return taskArray;
   }
 
-  getNextPosition(): number {
-    if (this.data === '') {
-      return 1;
-    } else {
-      return (
-        1 + parseInt(this.stringTaskArray[this.stringTaskArray.length - 1])
-      );
+  saveTaskArray(dataToSave: Task[]): void {
+    let lines: string[] = [];
+    for (let taskElement of dataToSave) {
+      lines.push(this.convertTaskToString(taskElement));
     }
-  }
-
-  addTask(description: string): void {
-    let nextPosition: number = this.getNextPosition();
-
-    fs.appendFileSync(this.source, `${nextPosition}\t${description}\tfalse\n`);
+    let outputData: string = lines.join('\n');
+    fs.writeFileSync(this.source, outputData);
     this.updateData();
-  }
-
-  removeTask(position: number): void {
-    if (position > this.stringTaskArray.length) {
-      throw new Error('Unable to remove: index is out of bound');
-    } else {
-      let indexToRemove: number = position - 1;
-
-      this.stringTaskArray.splice(indexToRemove, 1);
-    }
-    this.rearrangePositions();
-  }
-
-  rearrangePositions(): void {
-    let dataToWrite: string = '';
-    for (let i: number = 0; i < this.stringTaskArray.length; i++) {
-      let splitted: string[] = this.stringTaskArray[i].split('\t');
-      splitted[0] = '' + (i + 1);
-      this.stringTaskArray[i] = splitted.join('\t');
-      dataToWrite += this.stringTaskArray[i] + '\n';
-    }
-    fs.writeFileSync(this.source, dataToWrite);
-
-    this.updateData();
-  }
-
-  updateData() {
-    this.data = fs.readFileSync(this.source, 'utf-8').slice(0, -1);
-    this.stringTaskArray = this.data.split('\n');
-  }
-
-  checkTask(position: number): void {
-    // cheks task at position given
-  }
-
-  uncheck(position: number): void {
-    // unchecks task at position given
   }
 }
-
-const myFileHandler: FileHandler = new FileHandler('src/my-list.txt');
-myFileHandler.addTask('Test this');
-myFileHandler.addTask('Walk the dog');
-myFileHandler.removeTask(4);
